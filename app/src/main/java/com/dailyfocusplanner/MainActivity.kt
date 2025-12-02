93ab7e4651ee0f4886fbf2c596baf55f7521ba72
Hetanshi Kachhiya Patel <98518376+hetanshipatel@users.noreply.github.com>
2025-12-02 01:58:14 +0000

    Update MainActivity.kt

--- app/src/main/java/com/dailyfocusplanner/MainActivity.kt
+++ app/src/main/java/com/dailyfocusplanner/MainActivity.kt
@@ -1,99 +1,15 @@
-package com.example.dailyfocusplanner
+package com.dailyfocusplanner
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.dailyfocusplanner.ui.theme.DailyFocusPlannerTheme
+import com.dailyfocusplanner.ui.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            DailyFocusPlannerTheme {
-                AppNavigation()
-            }
+            NavGraph()
         }
     }
 }
-
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-
-    NavHost(
-        navController = navController,
-        startDestination = "welcome"
-    ) {
-        composable("welcome") { WelcomeScreen(navController) }
-        composable("home") { HomeScreen(navController) }
-        composable("tasks") { TasksScreen() }
-    }
-
-}
-
-@Composable
-fun WelcomeScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Welcome, Love ❤️", fontSize = 28.sp)
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(onClick = { navController.navigate("home") }) {
-            Text(text = "Continue")
-        }
-    }
-}
-
-@Composable
-fun HomeScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Your Daily Dashboard", fontSize = 24.sp)
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(onClick = { navController.navigate("tasks") }) {
-            Text("Go to Tasks")
-        }
-    }
-}
-
-@Composable
-fun TasksScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Tasks Page", fontSize = 26.sp)
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(text = "Here you will add and view tasks", fontSize = 16.sp)
-    }
-}
-
-
