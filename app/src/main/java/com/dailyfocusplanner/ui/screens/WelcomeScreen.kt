679754df4fe9eadea891568c7931d247873980f8
Hetanshi Kachhiya Patel <98518376+hetanshipatel@users.noreply.github.com>
2025-12-02 01:55:42 +0000

    Update WelcomeScreen.kt

--- app/src/main/java/com/dailyfocusplanner/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/dailyfocusplanner/ui/screens/WelcomeScreen.kt
@@ -1,1 +1,28 @@
+package com.dailyfocusplanner.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun WelcomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome, Love ❤️", fontSize = 28.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { navController.navigate("home") }) {
+            Text("Continue")
+        }
+    }
+}
 
