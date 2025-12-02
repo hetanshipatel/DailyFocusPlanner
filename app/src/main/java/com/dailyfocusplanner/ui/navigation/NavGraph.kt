dc0747ca7cc374b0e435d71caa0245ecae8b6b6d
Hetanshi Kachhiya Patel <98518376+hetanshipatel@users.noreply.github.com>
2025-12-02 01:57:10 +0000

    Update NavGraph.kt

--- app/src/main/java/com/dailyfocusplanner/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/dailyfocusplanner/ui/navigation/NavGraph.kt
@@ -1,1 +1,20 @@
+package com.dailyfocusplanner.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.dailyfocusplanner.ui.screens.HomeScreen
+import com.dailyfocusplanner.ui.screens.TasksScreen
+import com.dailyfocusplanner.ui.screens.WelcomeScreen
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "welcome") {
+        composable("welcome") { WelcomeScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+        composable("tasks") { TasksScreen() }
+    }
+}
 
