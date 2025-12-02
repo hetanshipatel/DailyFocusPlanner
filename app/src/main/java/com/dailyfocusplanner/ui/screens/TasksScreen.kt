0935a96fd38484b3bc94209c551e3f6c3024cd53
Hetanshi Kachhiya Patel <98518376+hetanshipatel@users.noreply.github.com>
2025-12-02 01:56:27 +0000

    Update TasksScreen.kt

--- app/src/main/java/com/dailyfocusplanner/ui/screens/TasksScreen.kt
+++ app/src/main/java/com/dailyfocusplanner/ui/screens/TasksScreen.kt
@@ -1,1 +1,24 @@
+package com.dailyfocusplanner.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TasksScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Tasks Page", fontSize = 26.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Here you will add and view tasks", fontSize = 16.sp)
+    }
+}
 
