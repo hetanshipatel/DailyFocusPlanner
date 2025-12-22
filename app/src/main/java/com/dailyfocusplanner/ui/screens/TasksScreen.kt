f1673ae998804cc857d96cf2339647fd66fc2045
Hetanshi Kachhiya Patel <98518376+hetanshipatel@users.noreply.github.com>
2025-12-22 22:18:58 +0000

    Update TasksScreen.kt

--- app/src/main/java/com/dailyfocusplanner/ui/screens/TasksScreen.kt
+++ app/src/main/java/com/dailyfocusplanner/ui/screens/TasksScreen.kt
@@ -1,24 +1,77 @@
 package com.dailyfocusplanner.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dailyfocusplanner.data.model.Task
 
 @Composable
 fun TasksScreen() {
+
+    // like: tasks = []
+    val tasks = remember { mutableStateListOf<Task>() }
+
+    // like: currentInput = ""
+    var taskText by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(16.dp)
     ) {
-        Text(text = "Tasks Page", fontSize = 26.sp)
+
+        Text(
+            text = "My Tasks",
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // INPUT FIELD
+        TextField(
+            value = taskText,
+            onValueChange = { taskText = it },
+            placeholder = { Text("Enter a task") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Here you will add and view tasks", fontSize = 16.sp)
+
+        // ADD BUTTON
+        Button(
+            onClick = {
+                if (taskText.isNotBlank()) {
+                    tasks.add(
+                        Task(
+                            id = tasks.size + 1,
+                            title = taskText
+                        )
+                    )
+                    taskText = ""
+                }
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Add")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // TASK LIST
+        LazyColumn {
+            items(tasks) { task ->
+                Text(
+                    text = "• ${task.title}",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
     }
 }
-
